d4f05a3c6b870c94cba6d9f116cd8cd07a68df4b
gustavsgoldbergs <75438804+gustavsgoldbergs@users.noreply.github.com>
2025-09-03 19:45:02 +0300

    Update Device.cs

--- Toolkit/Classes/Device.cs
+++ Toolkit/Classes/Device.cs
@@ -1,41 +1,86 @@
-using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
+/*
+ * Unlock.cs - Developed by Max Röhrl for Transformer Toolkit
+ */
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
-[assembly: AssemblyTitle("Transformer Toolkit")]
-[assembly: AssemblyDescription("A toolkit for the TF700T, TF300T, TF300TG, ME301T and the N5")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Max Röhrl")]
-[assembly: AssemblyProduct("Transformer Toolkit")]
-[assembly: AssemblyCopyright("Copyright © Max Röhrl  2016, Modified by Gustavs Goldbergs using Antropic Claude")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+namespace Toolkit
+{
+    internal static class Unlock
+    {
+        private static readonly string UnlockerPath = ResourceManager.TempPath + "Unlocker.apk";
+        private static Toolkit _toolkit;
+        private static Device _device;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+        public static void RequestUnlock(Toolkit toolkit, Device device)
+        {
+            _toolkit = toolkit;
+            _device = device;
 
-[assembly: ComVisible(false)]
+            switch (device.CodeName)
+            {
+                case "hammerhead":
+                case "hammerheadcaf":
+                    UnlockNexusBootloader();
+                    break;
+                case "tf700t":
+                case "tf300t":
+                case "tf300tg":
+                case "me301t":
+                    toolkit.InvokeLog("Downloading Asus Unlock Tool v8 ...");
+                    NetManager.DownloadFileAsync(NetManager.GetUnlockerUrl(), UnlockerPath, _toolkit.InvokeLog, InstallUnlocker);
+                    break;
+                default:
+                    toolkit.InvokeLogError("Unlocking your device is not possible with this tool");
+                    toolkit.InvokeToggleButtons(true);
+                    break;
+            }
+        }
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+        private static void UnlockNexusBootloader()
+        {
+            _toolkit.InvokeLog("Rebooting into bootloader ...");
+            Adb.ExecuteCommand("reboot bootloader", _device);
 
-[assembly: Guid("873db6df-3368-406b-bc7b-08efa89658ca")]
+            _toolkit.InvokeLog("Requesting unlock ...");
+            _toolkit.InvokeLog(Fastboot.ExecuteCommand("oem unlock", _device));
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+            _toolkit.ShowMessageBox("Now please follow the instructions on your device.");
 
-[assembly: AssemblyVersion("4.1.2")]
-[assembly: AssemblyFileVersion("4.1.2")]
-[assembly: NeutralResourcesLanguage("en")]
+            _toolkit.InvokeLog("Unlock requested!");
+            _toolkit.InvokeToggleButtons(true);
+        }
+
+        private static void InstallUnlocker()
+        {
+            _toolkit.InvokeLog("Installing Asus Unlock Tool v8 ...");
+            var output = Adb.ExecuteCommand($"install \"{UnlockerPath}\"", _device);
+            File.Delete(UnlockerPath);
+            _toolkit.InvokeLog(output);
+
+            if (output.Contains("Failure"))
+            {
+                _toolkit.InvokeLogError("Installation failed!");
+                _toolkit.InvokeToggleButtons(true);
+                return;
+            }
+            _toolkit.InvokeLog("Starting unlock app ...");
+            Adb.ExecuteShellCommand("am start -n com.asus.unlock/com.asus.unlock.EulaActivity", _device);
+            _toolkit.InvokeLog("Installation finished!");
+
+            _toolkit.ShowMessageBox("Now you can start the unlock app on your device.\r\n" +
+                                    "Please read the information carefully and follow the instructions there.\r\n" +
+                                    "When your device is unlocked you can flash a recovery.");
+
+            var dialogResult = _toolkit.ShowMessageBoxYesNo("After you unlocked your device you should generate \r\n" +
+                                                            "your nvflash blobs to make it unbrickable.\r\n" +
+                                                            "Would you like to visit the website of AndroidRoot.Mobi?");
+            if (dialogResult == DialogResult.Yes)
+                Process.Start("https://www.androidroot.mobi/pages/guides/tegra3-guide-nvflash-jellybean/");
+
+            _toolkit.InvokeToggleButtons(true);
+        }
+    }
+}
